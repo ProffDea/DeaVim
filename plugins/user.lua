00145a6b6cf7e87e1377219fd06c7fd1c520cda9
ProffDea <deatea@riseup.net>
2023-05-05 14:17:50 -0700

    catppuccin, trouble, rpc, lsp

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,27 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "andweeb/presence.nvim",
+    config = function()
+      require("presence").setup({
+        neovim_image_text = "The Banana Editor",
+        main_image = "file",
+      })
+    end,
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup()
+    end,
+  },
 }
