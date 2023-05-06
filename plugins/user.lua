3a227daa011281ac09edfc097f0e3a6c4c1137dd
ProffDea <deatea@riseup.net>
2023-05-06 03:25:49 -0700

    plugin: gitsigns line blame

--- plugins/user.lua
+++ plugins/user.lua
@@ -24,4 +24,16 @@ return {
       require("trouble").setup()
     end,
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup({
+        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          delay = 300,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%m/%d/%Y> - <summary>',
+      })
+    end,
+  },
 }
