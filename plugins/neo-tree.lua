c1cbfeef8cffabda63382f28e9a792950338f120
ProffDea <deatea@riseup.net>
2023-05-05 19:36:31 -0700

    relative line numbering

---
+++ plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function(_) vim.cmd [[ setlocal relativenumber ]] end,
+      },
+    },
+  }
+}
