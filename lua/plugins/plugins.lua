564ae2a65c8b7905d3b4342d85e6abc43785d23d
Dea <deatea@riseup.net>
2024-01-28 08:08:04 -0800

    oil show hidden files

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -62,7 +62,11 @@ return {
     "stevearc/oil.nvim",
     opts = {},
     config = function()
-      require("oil").setup()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
     end,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
