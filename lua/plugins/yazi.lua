d6e487d0f26adc22589a6fb77c2cf1b1d3bfd01d
Dea <deatea@riseup.net>
2024-06-27 16:14:51 -0700

    chore: replace oil with yazi

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>o",
+        function()
+          require("yazi").yazi()
+        end,
+        desc = "Open the file manager",
+      },
+      {
+        "<leader>cw",
+        function()
+          require("yazi").yazi(nil, vim.fn.getcwd())
+        end,
+        desc = "Open the file manager in nvim's working directory",
+      },
+    },
+    opts = {
+      open_for_directories = false,
+    },
+  },
+}
